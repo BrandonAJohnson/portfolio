4980e0ff829bf7a1b031d2b9b4bbd3db03a7d1fa
Brandon Johnson <brandonajohnson09@gmailcom>
2024-05-27 17:28:56 -0400

    WIP

---
+++ src/components/Header.svelte
@@ -0,0 +1,37 @@
+<script>
+ 	export let y;
+
+	let tabs = [
+		{
+			name: 'About Me',
+			link: '#aboutme'
+		},
+		{
+			name: 'Projects',
+			link: '#projects'
+		},
+		{
+			name: 'Blog',
+			link: '#blog'
+		},
+		{
+			name: 'Links',
+			link: '#links'
+		},
+	]
+</script>
+
+<header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + ((y > 0) ? ' py-4 bg-slate-950 border-slate-950 ' : ' py-6 bg-transparent border-transparent ')}>
+	<h1 class="text-4xl font-medium"><b class="font-bold">Brandon</b> Johnson</h1>
+	<div class="sm:flex items-center gap-4 hidden">
+		{#each tabs as tab, i}
+			<a class="duration-200 hover:text-amber-400" href={tab.link}>
+				<p>{tab.name}</p>
+			</a>
+		{/each}
+	</div>
+	<a href="#" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-blue-900 text-white">
+		<div class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-50 group-hover:translate-x-full z-0 duration-200"></div>
+		<h4 class="relative z-9">Contact Me</h4>
+	</a>
+</header>
---
+++ src/components/Main.svelte
@@ -0,0 +1,58 @@
+<script>
+	const favTech = [
+		{
+			name: 'HTML',
+			image: '/images/html5.png',
+		},
+		{
+			name: 'CSS',
+			image: '/images/css.jpg',
+		},
+		{
+			name: 'JavaScript',
+			image: '/images/javascript.png',
+		},
+		{
+			name: 'TypeScript',
+			image: '/images/typescript.png',
+		},
+		{
+			name: 'Svelte',
+			image: '/images/svelte.jpg',
+		},
+		{
+			name: 'TailwindCSS',
+			image: '/images/tailwind.jpg',
+		},
+		{
+			name: 'Node.js',
+			image: '/images/nodejs.png',
+		},
+		{
+			name: 'SQL',
+			image: '/images/sql.png',
+		},
+		{
+			name: 'Python',
+			image: '/images/python.png',
+		},
+	];
+</script>
+
+<main class="flex flex-col flex-1">
+	<section id="aboutme" class="grid grid-cols-1 lg:grid-cols-1 gap-10 py-8 sm:py-14">
+		<div class="flex flex-col lg:justify-center text-center lg:text-left gap-6 md:gap-8 lg:gap-10">
+			<h2 class="font-semibold text-3xl sm:text-4xl md:text-5xl">
+				Hello! I'm <span class="text-yellow-400">Brandon</span> Johnson<br>
+				Full Stack <span class="text-yellow-400">Developer</span>
+			</h2>
+			<p class="text-base sm:text-lg md:text-xl">
+				My <span class="text-yellow-400">preferred tech</span> includes
+				{#each favTech as tech, i}
+					{' '}{tech.name}
+					{#if tech.image}<img src={tech.image} alt={tech.name} class="h-8 inline-block"/>{/if}
+				{/each}
+			</p>
+		</div>
+	</section>
+</main>
