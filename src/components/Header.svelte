01db4fc86d3c2342b69864567e90853439db75aa
Brandon Johnson <brandonajohnson09@gmailcom>
2024-06-03 23:22:46 -0400

    remove blog for now, added contact info

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -14,10 +14,10 @@
 			name: 'About Me',
 			link: '#aboutme'
 		},
-		{
-			name: 'Blog',
-			link: '#blog'
-		},
+		// {
+		// 	name: 'Blog',
+		// 	link: '#blog'
+		// },
 		{
 			name: 'Links',
 			link: '#links'
@@ -26,15 +26,15 @@
 </script>
 
 <header class={'sticky z-[10] top-0 duration-200 px-6 flex items-center justify-between border border-solid ' + ((y > 0) ? ' py-4 bg-slate-950 border-slate-950 ' : ' py-6 bg-transparent border-transparent ')}>
-	<h1 class="text-4xl"><img src='/images/me-transparent.png' alt='Brandon Johnson' class="h-20 w-20 inline-block"/> <b class="font-extrabold">Brandon</b> Johnson</h1>
-	<div class="sm:flex ml-auto pr-4 items-center gap-4 hidden">
+	<h1 class="text-lg md:text-2xl lg:text-4xl"><img src='/images/me-transparent.png' alt='Brandon Johnson' class="h-20 w-20 inline-block"/> <b class="font-extrabold">Brandon</b> Johnson</h1>
+	<div class="md:flex ml-auto pr-4 items-center gap-4 hidden">
 		{#each tabs as tab, i}
 			<a class="duration-200 hover:text-amber-400" href={tab.link}>
 				<p>{tab.name}</p>
 			</a>
 		{/each}
 	</div>
-	<a href="#" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-blue-900 text-white">
+	<a href="mailto:brandonajohnson09@gmail.com" target="_blank" class="blueShadow relative overflow-hidden px-5 py-2 group rounded-full bg-blue-900 text-white">
 		<div class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-50 group-hover:translate-x-full z-0 duration-200"></div>
 		<h4 class="relative z-9">Contact Me</h4>
 	</a>
--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -30,6 +30,12 @@
 			content: 'Web based implementation of AI Steering Behaviors',
 		},
 		{
+			name: 'Machine Learning / Data Analysis',
+			href: 'https://github.com/BrandonAJohnson/machine-learning',
+			icon: 'fa-solid fa-ghost',
+			content: 'Jupyter Notebooks with data analysis',
+		},
+		{
 			name: 'Deep Learning Pacman',
 			href: '', // insert github url
 			icon: 'fa-solid fa-ghost',
@@ -187,7 +193,7 @@
 					{/if}
 				{/each}
 			</p>
-			<a href="mailto:brandonajohnson09@gmail.com" target="_blank" class="blueShadow mx-auto lg:mr-auto text-base sm:text-lg md:text-xl relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
+			<a href="mailto:brandonajohnson09@gmail.com" target="_blank" class="blueShadow mx-auto lg:mr-auto lg:ml-0 text-base sm:text-lg md:text-xl relative overflow-hidden px-6 py-3 group rounded-full bg-white text-slate-950 cursor-pointer">
 				<div class="absolute top-0 right-full w-full h-full bg-blue-400 opacity-50 group-hover:translate-x-full z-0 duration-200"></div>
 				<h4 class="relative z-9">Contact Me &rarr;</h4>
 			</a>
@@ -218,92 +224,94 @@
 	<section id="aboutme" class="py-20 pt-10 lg:pt-16 lg:py-32 flex flex-col gap-16 sm:gap-20 md:gap-24 relative">
 		<div class="flex flex-col gap-2 relative before:absolute before:top-0 before:left-0 before:w-2/3 before:h-1.5 before:bg-blue-700
 			after:absolute after:bottom-0 after:right-0 after:w-2/3 after:h-1.5 after:bg-blue-700 py-6">
-			<h6 class="text-lg sm:text-xl md:text-2xl mx-auto">Want to know more?</h6>
-			<p class="mx-auto font-semibold text-lg sm:text-xl md:text-2xl mb-8 text-amber-400">I am...</p>
-			<div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
-				{#each skills as skill, index}
-					<div class="flex gap-6 sm:gap-8">
-						<p class="text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semi-bold">{index<9?0:''}{index+1}</p>
-						<div class="flex flex-col gap-6 sm:gap-8">
-							<h3 class="text-2xl sm:text-3xl md:5xl">
-								{#if skill.image}
-									<img src={skill.image} alt={skill.name} class="inline-block h-10 w-auto"/>
-								{/if}
-								{skill.name}
-							</h3>
-							<p>{skill.description}</p>
-						</div>
+			<h6 class="text-lg sm:text-xl md:text-2xl mx-auto text-amber-400">Want to know more?</h6>
+		</div>
+		<p class="mx-auto font-semibold text-lg sm:text-xl md:text-2xl mb-8">I am...</p>
+		<div class="flex flex-col gap-20 w-full mx-auto max-w-[800px]">
+			{#each skills as skill, index}
+				<div class="flex gap-6 sm:gap-8">
+					<p class="text-4xl sm:text-5xl md:text-6xl text-slate-500 font-semi-bold">{index<9?0:''}{index+1}</p>
+					<div class="flex flex-col gap-6 sm:gap-8">
+						<h3 class="text-2xl sm:text-3xl md:5xl">
+							{#if skill.image}
+								<img src={skill.image} alt={skill.name} class="inline-block h-10 w-auto"/>
+							{/if}
+							{skill.name}
+						</h3>
+						<p>{skill.description}</p>
 					</div>
-				{/each}
-			</div>
-			<h5 class="text-2xl sm:text-3xl font-semibold text-center mt-10">My <span class="text-bold text-amber-400">Experience</span> and <span class="text-bold text-blue-400">Skill Set</span></h5>
-			<div class="mb-8 flex flex-col gap-2">
-				<Chart options={options} type={type} data={data}></Chart>
-			</div>
-			<div class="flex flex-col overflow-x-scroll gap-10 max-x-[800px] mx-auto w-full">
-				<table class="bg-white text-slate-700 rounded text-center">
-					<thead class="border-b border-solid border-slate-200">
-						<tr>
-							<th></th>
-							<th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
-							<th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
-							<th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
-							<th class="whitespace-nowrap p-2 px-4 bg-blue-700 text-white">Me</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr class="border-b border-solid border-slate-200">
-							<td class="border-r border-solid border-white pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Programming</td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
-						</tr>
-						<tr class="border-b border-solid border-slate-200">
-							<td class="border-r border-solid border-white pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Leadership</td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
-						</tr>
-						<tr class="border-b border-solid border-slate-200">
-							<td class="border-r border-solid border-white pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Critical Thinking</td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
-						</tr>
-						<tr class="border-b border-solid border-slate-200">
-							<td class="border-r border-solid border-white pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Analysis & Design</td>
-							<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-							<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
-						</tr>
-						<tr class="border-b border-solid border-slate-200">
-							<td class="border-r border-solid border-white pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Training & Mentoring</td>
-							<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-slate-500"></i></td>
-							<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
-			<div class="mx-auto -mt-12 italic sm:hidden opacity-50">
+				</div>
+			{/each}
+		</div>
+		<h5 class="text-2xl sm:text-3xl font-semibold text-center mt-10">My <span class="text-bold text-amber-400">Experience</span> and <span class="text-bold text-blue-400">Skill Set</span></h5>
+		<div class="mb-8 flex flex-col gap-2">
+			<Chart options={options} type={type} data={data}></Chart>
+		</div>
+		<div class="flex flex-col overflow-x-scroll gap-10 max-x-[800px] mx-auto w-full">
+			<table class="bg-white text-slate-700 rounded text-center">
+				<thead class="border-b border-solid border-slate-200">
+					<tr>
+						<th></th>
+						<th class="whitespace-nowrap p-2 px-4">Candidate #1</th>
+						<th class="whitespace-nowrap p-2 px-4">Candidate #2</th>
+						<th class="whitespace-nowrap p-2 px-4">Candidate #3</th>
+						<th class="whitespace-nowrap p-2 px-4 bg-blue-700 text-white">Me</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr class="border-b border-solid border-slate-200">
+						<td class="border-r border-solid border-slate-200 pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Programming</td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td class="border-r border-solid border-slate-200 pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Leadership</td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td class="border-r border-solid border-slate-200 pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Critical Thinking</td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td class="border-r border-solid border-slate-200 pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Analysis & Design</td>
+						<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+						<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
+					</tr>
+					<tr class="border-b border-solid border-slate-200">
+						<td class="border-r border-solid border-slate-200 pl-4 pr-8 py-4 font-bold text-sm whitespace-nowrap">Training & Mentoring</td>
+						<td><i class="fa-solid fa-xmark text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-slate-500"></i></td>
+						<td><i class="fa-solid fa-check text-green-500 text-2xl"></i></td>
+					</tr>
+				</tbody>
+			</table>
+			<div class="mx-auto -mt-10 p-2 italic sm:hidden opacity-50">
 				<p>Scroll to see more &rarr;</p>
 			</div>
 		</div>
 	</section>
-	<section id="blog" class="py-20 lg:py-32 flex flex-col gap-24">
+	<!-- <section id="blog" class="py-20 lg:py-32 flex flex-col gap-24">
 		<div class="flex flex-col text-center">
 			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">Some random thoughts</h3>
 		</div>
-	</section>
+	</section> -->
 	<section id="links" class="py-20 lg:py-32 flex flex-col gap-24">
 		<div class="flex flex-col text-center">
 			<h3 class="font-semibold text-3xl sm:text-4xl md:text-5xl">Where you can find me</h3>
-			<h3 class="m-8"><a href="https://www.linkedin.com/in/brandon-johnson-662256203/" target="_blank" class="inline-block"><img class="h-16 w-auto" src="/images/linkedin.png" alt="linkedin"/></a></h3>
+			<div class="m-8">
+				<a href="https://www.linkedin.com/in/brandon-johnson-662256203/" target="_blank" class="inline-block"><img class="h-16 w-auto" src="/images/linkedin.png" alt="linkedin"/></a>
+			</div>
 		</div>
 	</section>
 </main>
